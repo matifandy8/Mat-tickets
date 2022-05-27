9c054b063075a8cf83be77bcba6aa6b1046ee3d5
matias <matifandy@gmail.com>
2022-05-27 20:13:03 -0300

    lazy routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Fragment, Suspense } from "react";
+
 
 import "./App.css";
 import { AuthProvider } from "./contexts/Auth";
 import Layout from "./Layout";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Ticket from "./Pages/Ticket/Ticket";
+const Ticket = lazy(() => import("./Pages/Ticket/Ticket"));
+const Home = lazy(() => import("./Pages/Home"));
+const Login = lazy(() => import("./Pages/Login"));
+
+
+
+
 
 function App() {
   return (
     <AuthProvider>
       <Layout>
         <BrowserRouter>
+        <Suspense fallback={<p>Loading ...</p>}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="tickets/:id" element={<Ticket />} />
             <Route path="login" element={<Login />} />
           </Routes>
+        </Suspense>
         </BrowserRouter>
       </Layout>
     </AuthProvider>
